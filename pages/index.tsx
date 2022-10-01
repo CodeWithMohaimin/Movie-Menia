040b738be6699bb5909c9fbd69cd941f4e487060
programmermohaimin <you@example.com>
2022-10-01 18:10:27 +0600

    use state

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,28 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
+
+const posts = [
+  { title: 'React Testing', excerpt: 'Learn React Testing' },
+  { title: 'React with Tailwind ', excerpt: 'Learn React with Tailwind' },
+];
+
 const Home: NextPage = () => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>Create Next App</title>
+        <title>Mohaimin Blog Application</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <div className='bg-gray-200'>
+        {posts.map((post, index) => (
+          <div className='grid grid-cols-1 lg:grid-cols-12'>
+            {post.title}
+            {post.excerpt}
+          </div>
+        ))}
+      </div>
      
     </div>
   )
