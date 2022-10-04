06499f87ca2b9c5ec792c2e4e9d96dfede23bb68
programmermohaimin <dev.mohaimin@gmail.com>
2022-10-04 09:16:25 +0600

    problem solved

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { getPosts } from '../services';
 
 
 
-export default function Home(){
+export default function Home({posts}){
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
