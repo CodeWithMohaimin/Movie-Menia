a4d8efa61eadcd4e300c602c534d40d12c079cb3
programmermohaimin <dev.mohaimin@gmail.com>
2022-10-08 06:43:42 +0600

    post widget

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home({posts}){
         </div>
 
         <div className='lg:col-span-4 col-span-1'>
-          <div className='lg:sticky relative top-8 lg:px-6'>
+          <div className='lg:sticky relative top-8 lg:pl-8'>
             <PostWidget />
             <Categories />
           </div>
