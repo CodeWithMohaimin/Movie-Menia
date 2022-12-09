69977bcc17c5bd62c43a0d94641d9c58019f694c
programmermohaimin <dev.mohaimin@gmail.com>
2022-12-09 11:30:45 +0600

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,12 @@ const API_URL = " http://www.omdbapi.com?apikey=464eaa46";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    setMovies(data);
+    setMovies(data.Search);
   };
 
   useEffect(() => {
@@ -23,13 +24,22 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search for Movies"
-          value="superman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
         />
-        <img src={searchIco} alt="Search" onClick={() => {}} />
+        <img
+          src={searchIco}
+          alt="Search"
+          onClick={() => {
+            searchMovies(searchTerm);
+          }}
+        />
       </div>
+
       {movies?.length > 0 ? (
-        <div className="container">
+        <div key={movies.imdbID} className="container">
           {movies.map((movie) => (
             <MovieCard movie={movie} />
           ))}
